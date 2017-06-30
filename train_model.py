e0a6f259ff5fb4d8aec853461ac817f2c3a8c4bc
dcarnino <deniscarnino@gmail.com>
2017-06-30 15:30:52 +0200

    add: stacked regressors.

--- train_model.py
+++ train_model.py
@@ -8,21 +8,114 @@
 #==============================================
 import sys
 import os
+import copy
 import numpy as np
 import pandas as pd
 from collections import defaultdict
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.ensemble import ExtraTreesRegressor
+from sklearn import model_selection, metrics
 from scipy import sparse
 #==============================================
 #                   Files
 #==============================================
+import define_model
 
 
 #==============================================
 #                   Functions
 #==============================================
+def fit_stacked_regressors(X_train, y_train, n_folds=5,
+                           add_raw_features=False, verbose=1):
+    """
+    Regressify with two layers of regressors.
+    """
+
+    ### Init cross-validation K-folds
+    cv = model_selection.KFold(n_splits=n_folds, shuffle=True)
+
+    ### Train first layer of regressors
+    if verbose >= 1: print("Training 1st layer...")
+    fold_cnt = 0
+    X_oritrain, X2_train, y2_train = [], [], []
+    for valtrain_index, valtest_index in cv.split(X_train):
+        fold_cnt += 1
+        if verbose >= 1: print("Processing fold number %d/%d..."%(fold_cnt,n_folds))
+        # split features and target labels
+        X_valtrain, X_valtest = X_train[valtrain_index], X_train[valtest_index]
+        y_valtrain, y_valtest = y_train[valtrain_index], y_train[valtest_index]
+        # fit classifiers
+        reg_list = copy.deepcopy(define_model.reg_first_layer)
+        X2_valpred = []
+        for reg in reg_list:
+            if verbose >= 2:
+                print("%s ... "%reg[0],)
+                sys.stdout.flush()
+            reg[1].fit(X_valtrain, y_valtrain)
+            y_valpred = reg[1].predict(X_valtest)
+            X2_valpred.append(y_valpred.reshape((-1,1)))
+            if verbose >= 3: print("(%.02f) "%(metrics.mean_squared_error(y_valtest, y_valpred)*100.),)
+        if verbose >= 2: print("")
+        # append to new features
+        X_oritrain.append(X_valtest)
+        X2_train.append(np.hstack(X2_valpred))
+        y2_train.extend(y_valtest)
+    X_oritrain = np.vstack(X_oritrain)
+    X2_train = np.vstack(X2_train)
+    # refit classifiers on all training data
+    print("Refitting 1st layer on all training data...")
+    reg_list = copy.deepcopy(define_model.reg_first_layer)
+    for reg in reg_list:
+        if verbose >= 2:
+            print("%s ... "%reg[0],)
+            sys.stdout.flush()
+        reg[1].fit(X_oritrain, y2_train)
+    if verbose >= 2: print("")
+
+    ### Init final layer
+    reg_final = copy.deepcopy(define_model.reg_final_layer)
+
+    ### Train final layer
+    if verbose >= 1: print("Training 2nd layer...")
+    if add_raw_features:
+            reg_final.fit(np.hstack([X_oritrain, X2_train]), y2_train)
+    else:
+            reg_final.fit(X2_train, y2_train)
+
+    return reg_list, reg_final
+
+
+
+
+
+def predict_stacked_regressors(X_test, reg_list, reg_final, add_raw_features=False, verbose=1):
+    """
+    Predict for two layers regressor.
+    """
+
+    ### Predict with both layers
+    # layer 1
+    if verbose >= 1: print("Predictions of 1st layer...")
+    X2_test = []
+    for reg in reg_list:
+        y_subpred = reg[1].predict(X_test)
+        X2_test.append(y_subpred.reshape((-1,1)))
+    X2_test = np.hstack(X2_test)
+    # layer 2
+    if verbose >= 1: print("Predictions of 2nd layer...")
+    if add_raw_features:
+        X3_test = np.hstack([X_test, X2_test])
+    else:
+        X3_test = X2_test
+    y_pred = reg_final.predict(X3_test)
+
+    return y_pred
+
+
+
+
+
 def main(verbose=1):
     """
     Main function.
@@ -78,12 +171,13 @@ def main(verbose=1):
 
     ### Train model
     if verbose >= 1: print("Train model...")
-    reg = ExtraTreesRegressor(n_estimators=1000, n_jobs=-1, verbose=verbose)
-    reg.fit(X_train, y_train)
+    reg_list, reg_final = fit_stacked_regressors(X_train, y_train,
+                          add_raw_features=False, verbose=verbose)
 
     ### Predict with model
     if verbose >= 1: print("Predict with model...")
-    y_pred = reg.predict(X_test)
+    y_pred = predict_stacked_regressors(X_test, reg_list, reg_final,
+             add_raw_features=False, verbose=verbose)
 
     ### Save predictions
     if verbose >= 1: print("Save predictions...")
@@ -102,4 +196,4 @@ def main(verbose=1):
 #                   Main
 #==============================================
 if __name__ == '__main__':
-    main(1)
+    main(3)
