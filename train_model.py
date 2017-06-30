f84990aa6d90327baa97d4a820f43b64ed51a934
dcarnino <deniscarnino@gmail.com>
2017-06-30 14:19:33 +0200

    add: first script with extratrees prediction.

---
+++ train_model.py
@@ -0,0 +1,97 @@
+"""
+    Name:           train_model.py
+    Created:        29/6/2017
+    Description:    Train model on mercedes data.
+"""
+#==============================================
+#                   Modules
+#==============================================
+import sys
+import os
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.ensemble import ExtraTreesRegressor
+#==============================================
+#                   Files
+#==============================================
+
+
+#==============================================
+#                   Functions
+#==============================================
+def main(verbose=1):
+    """
+    Main function.
+    """
+
+    ### Import data
+    if verbose >= 1: print("Import data...")
+    train_csv_name = "../data/mercedes/train.csv"
+    test_csv_name = "../data/mercedes/test.csv"
+    df_train = pd.read_csv(train_csv_name)
+    df_test = pd.read_csv(train_csv_name)
+    categorical_columns = ["X%d"%cid for cid in range(8)]
+    binary_columns = ["X%d"%cid for cid in range(8,386)]
+    id_train = df_train["ID"].values
+    y_train = df_train["y"].values
+    Xc_train = df_train[categorical_columns].values
+    Xb_train = df_train[binary_columns].values
+    id_test = df_train["ID"].values
+    Xc_test = df_test[categorical_columns].values
+    Xb_test = df_test[binary_columns].values
+    if verbose >= 3:
+        print("\tid_train shape: ", id_train.shape)
+        print("\ty_train shape: ", y_train.shape)
+        print("\tXc_train shape: ", Xc_train.shape)
+        print("\tXb_train shape: ", Xb_train.shape)
+        print("\tid_test shape: ", id_test.shape)
+        print("\tXc_test shape: ", Xc_test.shape)
+        print("\tXb_test shape: ", Xb_test.shape)
+
+    ### Extract features
+    if verbose >= 1: print("Extract features...")
+    # encode categorical
+    ohe = OneHotEncoder()
+    ohe.fit(Xc_train)
+    Xohe_train = ohe.transform(Xc_train)
+    Xohe_test = ohe.transform(Xc_test)
+    # merge all binary features
+    X_train = np.hstack([Xohe_train, Xb_train])
+    X_test = np.hstack([Xohe_test, Xb_test])
+    # remove constant
+    vt = VarianceThreshold()
+    vt.fit(X_train)
+    X_train = vt.transform(X_train)
+    X_test = vt.transform(X_test)
+    if verbose >= 3:
+        print("\tX_train shape: ", X_train.shape)
+        print("\tX_test shape: ", X_test.shape)
+
+    ### Train model
+    if verbose >= 1: print("Train model...")
+    reg = ExtraTreesRegressor(n_estimators=1000, n_jobs=-1)
+    reg.fit(X_train, y_train)
+
+    ### Predict with model
+    if verbose >= 1: print("Predict with model...")
+    y_pred = reg.predict(X_test)
+
+    ### Save predictions
+    if verbose >= 1: print("Save predictions...")
+    pred_csv_name = "../data/mercedes/pred.csv"
+    pred_df = pd.DataFrame([id_test, y_pred], columns=["ID", "y"])
+    pred_df.to_csv(pred_csv_name, index=False)
+
+
+
+
+
+
+
+#==============================================
+#                   Main
+#==============================================
+if __name__ == '__main__':
+    main(3)
