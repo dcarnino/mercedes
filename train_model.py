ba5f29dc6872ff9306aa62715c115475162d64ed
dcarnino <deniscarnino@gmail.com>
2017-07-02 18:45:58 +0200

    add: global cv.

--- train_model.py
+++ train_model.py
@@ -17,6 +17,7 @@ from sklearn.feature_selection import VarianceThreshold
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn import model_selection, metrics
 from scipy import sparse
+from xgboost import XGBRegressor
 #==============================================
 #                   Files
 #==============================================
@@ -145,27 +146,9 @@ def main(verbose=1):
     Xb_test = df_test.iloc[:,9:]
     Xc_test = df_test.iloc[:,1:9]
 
-    ### Init cross-validation K-folds
-    n_folds = 5
-    cv = model_selection.KFold(n_splits=n_folds, shuffle=True)
-
-    ### Split folds and fit+predict
-    fold_cnt = 0
-    for valtrain_index, valtest_index in cv.split(Xb_train.values):
-        fold_cnt += 1
-        if verbose >= 1: print("BIG CV: Processing fold number %d/%d..."%(fold_cnt,n_folds))
-        # split features and target labels
-        id_valtrain, id_valtest = id_train.iloc[valtrain_index], id_train.iloc[valtest_index]
-        y_valtrain, y_valtest = y_train.iloc[valtrain_index], y_train.iloc[valtest_index]
-        Xb_valtrain, Xb_valtest = Xb_train.iloc[valtrain_index], Xb_train.iloc[valtest_index]
-        Xc_valtrain, Xc_valtest = Xc_train.iloc[valtrain_index], Xc_train.iloc[valtest_index]
-        print(Xb_train.shape, Xb_valtrain.shape, y_train.shape, y_valtest.shape)
-        raise(ValueError)
-
-
     # string to numerical
     label_dict = defaultdict(LabelEncoder)
-    pd.concat([Xc_train,Xc_test]).apply(lambda x: label_dict[x.name].fit(x))
+    pd.concat([Xc_train,Xc_test]).apply(lambda x: label_dict[x.name].fit(x.sort_values()))
     Xc_train = Xc_train.apply(lambda x: label_dict[x.name].transform(x)).values
     Xc_test = Xc_test.apply(lambda x: label_dict[x.name].transform(x)).values
     if verbose >= 3:
@@ -177,45 +160,88 @@ def main(verbose=1):
         print("\tXc_test shape: ", Xc_test.shape)
         print("\tXb_test shape: ", Xb_test.shape)
 
-    ### Extract features
-    if verbose >= 1: print("Extract features...")
-    # encode categorical
-    ohe = OneHotEncoder(handle_unknown='ignore')
-    ohe.fit(Xc_train)
-    Xohe_train = ohe.transform(Xc_train).toarray()
-    Xohe_test = ohe.transform(Xc_test).toarray()
-    # merge all binary features
-    X_train = np.hstack([Xohe_train, Xb_train])
-    X_test = np.hstack([Xohe_test, Xb_test])
-    # remove constant
-    vt = VarianceThreshold()
-    vt.fit(X_train)
-    X_train = vt.transform(X_train)
-    X_test = vt.transform(X_test)
     # remove outlier
-    X_train = X_train[y_train < 200]
+    Xb_train = Xb_train[y_train < 200]
+    Xc_train = Xc_train[y_train < 200]
+    id_train = id_train[y_train < 200]
     y_train = y_train[y_train < 200]
-    if verbose >= 3:
-        print("\tX_train shape: ", X_train.shape)
-        print("\tX_test shape: ", X_test.shape)
+
+    ### Init cross-validation K-folds
+    n_folds = 5
+    cv = model_selection.KFold(n_splits=n_folds, shuffle=True)
+    y_trainpred, y_traintest = [], []
+
+    ### Split folds and fit+predict
+    fold_cnt = 0
+    for valtrain_index, valtest_index in cv.split(Xb_train.values):
+        fold_cnt += 1
+        if verbose >= 1: print("BIG CV: Processing fold number %d/%d..."%(fold_cnt,n_folds))
+        # split features and target labels
+        id_valtrain, id_valtest = id_train.iloc[valtrain_index].values, id_train.iloc[valtest_index].values
+        y_valtrain, y_valtest = y_train.iloc[valtrain_index].values, y_train.iloc[valtest_index].values
+        Xb_valtrain, Xb_valtest = Xb_train.iloc[valtrain_index].values, Xb_train.iloc[valtest_index].values
+        Xc_valtrain, Xc_valtest = Xc_train.iloc[valtrain_index], Xc_train.iloc[valtest_index]
+
+        ### Extract features
+        if verbose >= 1: print("Extract features...")
+        # encode categorical
+        ohe = OneHotEncoder(handle_unknown='ignore')
+        ohe.fit(Xc_valtrain)
+        Xohe_valtrain = ohe.transform(Xc_valtrain).toarray()
+        Xohe_valtest = ohe.transform(Xc_valtest).toarray()
+        # merge all binary features
+        X_valtrain = np.hstack([Xohe_valtrain, Xb_valtrain])
+        X_valtest = np.hstack([Xohe_valtest, Xb_valtest])
+        # remove constant
+        vt = VarianceThreshold()
+        vt.fit(X_valtrain)
+        X_valtrain = vt.transform(X_valtrain)
+        X_valtest = vt.transform(X_valtest)
+        if verbose >= 3:
+            print("\tX_valtrain shape: ", X_valtrain.shape)
+            print("\tX_valtest shape: ", X_valtest.shape)
 
 
-    ### Train model
-    if verbose >= 1: print("Train model...")
-    reg_list, reg_final = fit_stacked_regressors(X_train, y_train,
-                          add_raw_features=True, verbose=verbose)
+        ### Train model
+        if verbose >= 1: print("Train model...")
+        n_jobs = 28
+        reg = XGBRegressor(n_estimators=1120, objective='reg:linear', gamma=0, reg_lambda=1, min_child_weight=4,
+                           learning_rate=0.02, subsample=0.8, colsample_bytree=0.8, max_depth=4, nthread=n_jobs)
+        reg.fit(X_valtrain, y_valtrain)
+        #reg_list, reg_final = fit_stacked_regressors(X_valtrain, y_valtrain,
+        #                      add_raw_features=True, verbose=verbose)
 
-    ### Predict with model
-    if verbose >= 1: print("Predict with model...")
-    y_pred = predict_stacked_regressors(X_test, reg_list, reg_final,
-             add_raw_features=True, verbose=verbose)
+        ### Predict with model
+        if verbose >= 1: print("Predict with model...")
+        y_valpred = reg.predict(X_valtest)
+        #y_valpred = predict_stacked_regressors(X_valtest, reg_list, reg_final,
+        #            add_raw_features=True, verbose=verbose)
 
-    ### Save predictions
+        ### Append preds and tests
+        y_trainpred.extend(y_valpred)
+        y_traintest.extend(y_valtest)
+
+
+    ##### Compute R2-score
+    r2_score = metrics.r2_score(y_valtest, y_valpred)
+    print("FINAL CV R2: ", r2_score)
+
+    if verbose >= 1: print("Save predictions...")
+    trainpred_csv_name = "../data/mercedes/trainpred.csv"
+    trainpred_df = pd.DataFrame(np.array([y_trainpred]).T, columns=["y"])
+    trainpred_df.to_csv(trainpred_csv_name, index=False)
+    traintest_csv_name = "../data/mercedes/traintest.csv"
+    traintest_df = pd.DataFrame(np.array([y_traintest]).T, columns=["y"])
+    traintest_df.to_csv(traintest_csv_name, index=False)
+
+
+
+    """### Save predictions
     if verbose >= 1: print("Save predictions...")
     pred_csv_name = "../data/mercedes/pred.csv"
     pred_df = pd.DataFrame(np.array([id_test, y_pred]).T, columns=["ID", "y"])
     pred_df["ID"] = pred_df["ID"].astype(int)
-    pred_df.to_csv(pred_csv_name, index=False)
+    pred_df.to_csv(pred_csv_name, index=False)"""
 
 
 
