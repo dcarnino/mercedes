1f56623385e26db2b897e6955128f81d7408f33e
dcarnino <deniscarnino@gmail.com>
2017-07-03 19:19:12 +0200

    add: logpca in R

--- logpca.R
+++ logpca.R
@@ -20,11 +20,11 @@ binary_train <- read.csv(file="../data/mercedes/binary_train.csv", header=TRUE, 
 binary_test <- read.csv(file="../data/mercedes/binary_test.csv", header=TRUE, sep=",")
 
 print("Define parameters...")
-k <- 2; m <- 4
+k <- 20; m <- 12
 # print("Remove constant columns...")
 # binary_train <- binary_train[,apply(binary_train, 2, var, na.rm=TRUE) != 0]
 print("Fit model...")
-logpca_model = convexLogisticPCA(binary_train, k = k, m = m, quiet=FALSE, partial_decomp=TRUE, main_effects=FALSE)
+logpca_model = convexLogisticPCA(binary_train, k = k, m = m, quiet=FALSE, partial_decomp=TRUE, main_effects=FALSE, conv_criteria=1e-4)
 print("Predict on train...")
 logpca_features_train <- predict(logpca_model, newdata=binary_train); colnames(logpca_features_train) <- paste0("LPC", 1:k)
 print("Predict on test...")
