1d7f02a62c83071b4dc1770e8579de7808356339
dcarnino <deniscarnino@gmail.com>
2017-07-03 14:12:49 +0200

    add: y target transofmr to rank.

--- train_model.py
+++ train_model.py
@@ -305,9 +305,9 @@ def main(verbose=1):
             if verbose >= 4: print("Train model...")
             if fold_cnt+n_folds*ix_cv == 1:
                 reg_cv = model_selection.GridSearchCV(define_model.create_final_layer(n_jobs=28, n_est=1120, objective='reg:logistic', verbose=verbose),
-                                                   {'max_depth': [5,7], 'subsample': [.6,.8], 'colsample_bytree': [.6,.8], 'n_estimators': [112, 224, 448, 672]},
+                                                   {'max_depth': [4,5,6], 'subsample': [.5,.6,.7], 'colsample_bytree': [.5,.6,.7], 'n_estimators': [224]},
                                                    scoring=metrics.make_scorer(metrics.r2_score, greater_is_better=True),
-                                                   n_jobs=1, cv=5, verbose=2, pre_dispatch='n_jobs', error_score='raise')
+                                                   n_jobs=1, cv=5, verbose=3, pre_dispatch='n_jobs', error_score='raise')
                 reg_cv.fit(X_valtrain, y_valtrain)
                 print(reg_cv.best_params_, reg_cv.best_score_)
                 reg = reg_cv.best_estimator_
