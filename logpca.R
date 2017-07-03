6bf471f18cbe3015670dc1e4ecc9306158ccb0a6
dcarnino <deniscarnino@gmail.com>
2017-07-03 17:42:05 +0200

    add: logpca in R

--- logpca.R
+++ logpca.R
@@ -14,13 +14,13 @@ library(rARPACK)
 #  k       1        2      3        4        5        6        7        8        9       10
 #  20 400428 261586.6 185985 143663.3 118547.4 102668.9 92638.51 85579.33 80440.14 76707.54
 
-binary_train <- read_csv("../data/binary_train.csv")
-binary_test <- read_csv("../data/binary_test.csv")
+binary_train <- read_csv("../data/mercedes/binary_train.csv")
+binary_test <- read_csv("../data/mercedes/binary_test.csv")
 
 k <- 20; m <- 12
 logpca_model = logisticPCA(binary_train, k = k, m = m)
 logpca_features_train <- predict(logpca_model, newdata=binary_train); colnames(logpca_features_train) <- paste0("LPC", 1:k)
 logpca_features_test <- predict(logpca_model, newdata=binary_test); colnames(logpca_features_test) <- paste0("LPC", 1:k)
 
-write.csv(logpca_features_train, file="../data/logpca_train.csv", row.names=FALSE)
-write.csv(logpca_features_test, file="../data/logpca_test.csv", row.names=FALSE)
+write.csv(logpca_features_train, file="../data/mercedes/logpca_train.csv", row.names=FALSE)
+write.csv(logpca_features_test, file="../data/mercedes/logpca_test.csv", row.names=FALSE)
